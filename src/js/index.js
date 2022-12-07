656fe5cad37372baf687981d3bcf26b03891ef6a
Thundeee <asbjornabel@gmail.com>
2022-12-07 16:40:50 +0100

    adding avatar thingy need work still

--- src/js/index.js
+++ src/js/index.js
@@ -16,4 +16,6 @@ async function allTest() {
   // calls.updateAvatar("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg")
 }
 
+
+console.log(localStorage.getItem("avatar"))
 //allTest();
--- src/js/utils/domScripts/loginData.js
+++ src/js/utils/domScripts/loginData.js
@@ -6,3 +6,18 @@ localStorage.getItem("avatar");
 
 
 document.getElementById("wallet").innerHTML = "Wallet: " + localStorage.getItem("credits");
+
+let btn = document.querySelector("#loginButtonDiv");
+
+if (localStorage.getItem("avatar") && localStorage.getItem("avatar") != "null") {
+    console.log("avatar exists");
+    btn.innerHTML = `<img class="img-fluid" src="${localStorage.getItem("avatar")} height="100" width="100" " data-bs-toggle="modal" href="#logoutModal" alt="profile picture" style="cursor: pointer;"/>`;
+} else {
+    console.log("no avatar");
+
+}
+
+// USE DEFAULT PIC IF avatar === NULL
+
+
+// <!-- <img class="img-fluid col-2" src="./assets/img/noImg.jpg" data-bs-toggle="modal" href="#logoutModal" alt="profile picture" style="cursor: pointer;"/>-->
